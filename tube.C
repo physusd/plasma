b4580c8eba8bb98045593fc41c9c8c97b728df7d
Jing LIU <jing.liu@usd.edu>
2015-06-29 15:53:01 -0500

    used d(En)

--- tube.C
+++ tube.C
@@ -13,12 +13,20 @@ using namespace UNIC;
 // MAD
 #include <MAD/GeCrystal.h>
 using namespace MAD;
+const int N = 500;
+// df at i
+double d(double *f, int i)
+{
+   if (i<4 || i>2*N-4) return 0;
+   return (f[i+1]-f[i-1])/2; // 2 points
+   //return (-f[i+2]+8*f[i+1]-8*f[i-1]+f[i-2])/12; // 4
+   //return (-f[i+4]-8*f[i+2]+128*f[i+1]-128*f[i-1]+8*f[i-2]+f[i-4])/180; // 6
+}
 // thin plasma tube
-//GeCrystal *ge = new GeCrystal;
 int main(int argc, char** argv)
 {
    double dt=1e-7*ns; // large dt causes E to decrease too fast
-   double dx=0.5*nm; // large dx may cause asymmetry 
+   double dx=0.5*nm; // small dx may cause artificial oscillation
    double Ee=1000*volt/cm;
    double R=1e-6*cm, mean=0, sigma=R/3, height=300./(Pi()*R*R);
    bool norm;
@@ -26,51 +34,52 @@ int main(int argc, char** argv)
    GeCrystal ge;
    double epsilon=ge.Epsilon()*epsilon0;
    double Q=Abs(electron_charge);
-   double De=0.046*cm2/s, Dh=0.046*cm2/s;
+   //double De=0.046*cm2/s, Dh=0.046*cm2/s;
 
    // initialize arrays
-   const int N = 500;
    // p and n are number densities
    double x[2*N+1], p[2*N+1], n[2*N+1], E[2*N+1], pE[2*N+1],nE[2*N+1];
    for (int i=0; i<2*N+1; i++) {
       x[i]=(i-N)*dx;
       p[i]=n[i]=height*Gaus(x[i],mean=0,sigma,norm=kTRUE);
+      if (p[i]<1e-5/cm3) p[i]=0;
+      if (n[i]<1e-5/cm3) n[i]=0;
       E[i]=Ee;
       pE[i]=p[i]*E[i];
       nE[i]=n[i]*E[i];
    }
    // slopes
-   double dp[2*N+1], dn[2*N+1], dE[2*N+1], dpE[2*N+1], dnE[2*N+1];
-   for (int i=1; i<2*N; i++) {
-      dp[i] = (p[i+1]-p[i-1])/2;
-      dn[i] = (n[i+1]-n[i-1])/2;
-      dE[i] = (E[i+1]-E[i-1])/2;
-      dpE[i] = (pE[i+1]-pE[i-1])/2;
-      dnE[i] = (nE[i+1]-nE[i-1])/2;
-   }
-   dp[2*N] = dp[0] = 0;
-   dn[2*N] = dn[0] = 0;
-   dE[2*N] = dE[0] = 0;
-   dpE[2*N] = dpE[0] = 0;
-   dnE[2*N] = dnE[0] = 0;
+   //double dp[2*N+1], dn[2*N+1], dE[2*N+1], dpE[2*N+1], dnE[2*N+1];
+   //for (int i=1; i<2*N; i++) {
+   //   dp[i] = (p[i+1]-p[i-1])/2;
+   //   dn[i] = (n[i+1]-n[i-1])/2;
+   //   dE[i] = (-E[i+2]+8*E[i+1]-8*E[i-1]+E[i-2])/12;
+   //   dpE[i] = (-pE[i+2]+8*pE[i+1]-8*pE[i-1]+pE[i-2])/12;
+   //   dnE[i] = (-nE[i+2]+8*nE[i+1]-8*nE[i-1]+nE[i-2])/12;
+   //}
+   //dp[2*N] = dp[0] = 0;
+   //dn[2*N] = dn[0] = 0;
+   //dE[2*N] = dE[0] = 0;
+   //dpE[2*N] = dpE[0] = 0;
+   //dnE[2*N] = dnE[0] = 0;
 
-   double d2p[2*N+1], d2n[2*N+1];
-   for (int i=1; i<2*N; i++) {
-      d2p[i] = (dp[i+1]-dp[i-1])/2;
-      d2n[i] = (dn[i+1]-dn[i-1])/2;
-   }
-   d2p[2*N] = d2p[0] = 0;
-   d2n[2*N] = d2n[0] = 0;
+   //double d2p[2*N+1], d2n[2*N+1];
+   //for (int i=1; i<2*N; i++) {
+   //   d2p[i] = (dp[i+1]-dp[i-1])/2;
+   //   d2n[i] = (dn[i+1]-dn[i-1])/2;
+   //}
+   //d2p[2*N] = d2p[0] = 0;
+   //d2n[2*N] = d2n[0] = 0;
 
-   double phi[2*N+1]; // weighting potential
-   for (int i=0; i<2*N+1; i++) phi[i]=float(i)/2/N;
+   //double phi[2*N+1]; // weighting potential
+   //for (int i=0; i<2*N+1; i++) phi[i]=float(i)/2/N;
 
-   //Transverse diffusion 
-   double Re[2*N+1], Rh[2*N+1];
-   for (int i=0; i<2*N+1; i++){
-      Re[i]=18*n[i]*De/R/R;
-      Rh[i]=18*p[i]*Dh/R/R;
-   }
+   ////Transverse diffusion 
+   //double Re[2*N+1], Rh[2*N+1];
+   //for (int i=0; i<2*N+1; i++){
+   //   Re[i]=18*n[i]*De/R/R;
+   //   Rh[i]=18*p[i]*Dh/R/R;
+   //}
 
    // output
    TFile *output = new TFile("tube.root","recreate");
@@ -98,34 +107,38 @@ int main(int argc, char** argv)
    t->Branch("cm2",&CM2,"cm2/D");
    t->Branch("V",&V,"V/D");
    t->Branch("cm",&CM,"cm/D");
-   //   t->Branch("um",&UM,"um/D");
+   t->Branch("um",&UM,"um/D");
    t->Branch("nm",&NM,"nm/D");
-   //   t->Branch("ns",&NS,"ns/D");
+   t->Branch("ns",&NS,"ns/D");
    t->Branch("s",&SEC,"s/D");
-   //   t->Branch("eps",&epsilon,"eps/D");
-   //   t->Branch("q",&Q,"q/D");
+//   t->Branch("eps",&epsilon,"eps/D");
+//   t->Branch("q",&Q,"q/D");
 
    int iStep=0, nSteps = 100;
-   double time[1000]={0}, charge[1000]={0};
+   //double time[1000]={0}, charge[1000]={0};
 
    // evolve
    if (argc>1) nSteps = atoi(argv[1]);
-//  t->Branch("mue",&mu_e,"mue/D");
-//  t->Branch("muh",&mu_h,"muh/D");
- 
    while (iStep<nSteps) {
+      if (iStep%100==0) cout<<iStep<<" steps passed"<<endl;
       t->Fill();
       // update charge
-      for (int i=0; i<2*N+1; i++) {
-         charge[iStep]+=(p[i]-n[i])*phi[i];
-      }
-      time[iStep]=iStep*dt;
+      //for (int i=0; i<2*N+1; i++) {
+      //   charge[iStep]+=(p[i]-n[i])*phi[i];
+      //}
+      //time[iStep]=iStep*dt;
       // update electron and hole distributions
       for (int i=0; i<2*N+1; i++) {
-         double dn_dt = ge.Mu('e', n[i])*(dn[i]/dx*E[i]+n[i]*dE[i]/dx);
-         double dp_dt = -ge.Mu('h', p[i])*(dp[i]/dx*E[i]+p[i]*dE[i]/dx);
+         //double dn_dt = ge.Mu('e', n[i])*(dn[i]/dx*E[i]+n[i]*dE[i]/dx);
+         //double dp_dt = -ge.Mu('h', p[i])*(dp[i]/dx*E[i]+p[i]*dE[i]/dx);
+         double dn_dt = ge.Mu('e', n[i])*d(nE,i)/dx;
+         double dp_dt = -ge.Mu('h', p[i])*d(pE,i)/dx;
+         if (Abs(dn_dt*dt)<1e-10/cm3) dn_dt=0;
+         if (Abs(dp_dt*dt)<1e-10/cm3) dp_dt=0;
          n[i]+=dn_dt*dt;
          p[i]+=dp_dt*dt;
+         if (n[i]<0) n[i]=0;
+         if (p[i]<0) p[i]=0;
       }
       // update electric field distribution
       for (int i=0; i<2*N+1; i++) {
@@ -135,52 +148,49 @@ int main(int argc, char** argv)
          for (int j=i+1; j<2*N+1; j++) 
             E[i]+=(n[j]-p[j])*(1-(j-i)*dx/sqrt((j-i)*(j-i)*dx*dx+R*R));
          E[i]*=Q*dx/2/epsilon;
-         if (iStep==(nSteps-1))
-            cout<<"E["<<i<<"] = "<<E[i]<<endl;
          E[i]+=Ee;
-         if (E[i]<0) E[i]=0; // large dt may over evolve things
+         if (E[i]<1e-10*volt/cm) E[i]=0; // large dt may over evolve things
       }
-      
+
       // update pE[i]and nE[i]
       for (int i=0; i<2*N+1; i++){
          pE[i] = p[i]*E[i];
          nE[i] = n[i]*E[i];
-
       }
       // update slopes
-      for (int i=1; i<2*N; i++) {
-         dp[i] = (p[i+1]-p[i-1])/2;
-         dn[i] = (n[i+1]-n[i-1])/2;
-         dE[i] = (E[i+1]-E[i-1])/2;
-         dpE[i] =(pE[i+1]-pE[i-1])/2;
-         dnE[i] =(nE[i+1]-nE[i-1])/2;
-      }
-      dp[2*N] = dp[0] = 0;
-      dn[2*N] = dn[0] = 0;
-      dE[2*N] = dE[0] = 0;
-      dpE[2*N] = dpE[0] = 0;
-      dnE[2*N] = dnE[0] = 0;
+      //for (int i=1; i<2*N; i++) {
+      //   dp[i] = (p[i+1]-p[i-1])/2;
+      //   dn[i] = (n[i+1]-n[i-1])/2;
+      //   dE[i] = (-E[i+2]+8*E[i+1]-8*E[i-1]+E[i-2])/12;
+      //   dpE[i] = (-pE[i+2]+8*pE[i+1]-8*pE[i-1]+pE[i-2])/12;
+      //   dnE[i] = (-nE[i+2]+8*nE[i+1]-8*nE[i-1]+nE[i-2])/12;
+      //}
+      //dp[2*N] = dp[0] = 0;
+      //dn[2*N] = dn[0] = 0;
+      //dE[2*N] = dE[0] = 0;
+      //dpE[2*N] = dpE[0] = 0;
+      //dnE[2*N] = dnE[0] = 0;
 
-      // update second derivative
-      for (int i=1; i<2*N; i++) {
-         d2p[i] = (dp[i+1]-dp[i-1])/2;
-         d2n[i] = (dn[i+1]-dn[i-1])/2;
-      }
-      d2p[2*N] = d2p[0] = 0;
-      d2n[2*N] = d2n[0] = 0;
+      //// update second derivative
+      //for (int i=1; i<2*N; i++) {
+      //   d2p[i] = (dp[i+1]-dp[i-1])/2;
+      //   d2n[i] = (dn[i+1]-dn[i-1])/2;
+      //}
+      //d2p[2*N] = d2p[0] = 0;
+      //d2n[2*N] = d2n[0] = 0;
 
-      //update transverse diffusion
-      for (int i=0; i<2*N+1; i++){
-         Re[i]=(18*n[i]*De/R/R)*((18*De*iStep*dt/R/R)+1);
-         Rh[i]=(18*p[i]*Dh/R/R)*((18*Dh*iStep*dt/R/R)+1);
-      }
+      ////update transverse diffusion
+      //for (int i=0; i<2*N+1; i++){
+      //   Re[i]=(18*n[i]*De/R/R)*((18*De*iStep*dt/R/R)+1);
+      //   Rh[i]=(18*p[i]*Dh/R/R)*((18*Dh*iStep*dt/R/R)+1);
+      //}
 
       iStep++;
    }
    t->Write("t",6);
 
-  // TGraph *g = new TGraph(nSteps,time,charge);
-  // g->Write("g");
+   // TGraph *g = new TGraph(nSteps,time,charge);
+   // g->Write("g");
 
    output->Close();
 
